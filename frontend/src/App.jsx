7764e397cecae2223507616ef5719fa5b17a4e3a
anonymous-gtx <anonymous-gtx@proton.me>
2025-02-02 17:05:32 +0530

    added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login.jsx";
+// import Register from "./pages/Register";
+// import Threats from "./pages/Threats";
+// import ThreatDetail from "./pages/ThreatDetail";
+// import SubmitThreat from "./pages/SubmitThreat";
+// import Agreements from "./pages/Agreements";
+
+function AppRouter() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                 {/* <Route path="/register" element={<Register />} />
+                <Route path="/threats" element={<Threats />} />
+                <Route path="/threats/:id" element={<ThreatDetail />} />
+                <Route path="/submit-threat" element={<SubmitThreat />} />
+                <Route path="/agreements" element={<Agreements />} /> */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
